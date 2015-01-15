8c3c3bc511dda6db7461eccdd686dd600657ea95
Renato Mussa <rfmussa@gmail.com>
2015-01-15 14:42:57 +0100

    Re-used adapter for adding removing locations

--- app/src/main/java/renato/weather/carrot/com/carrot/LocationAdapter.java
+++ app/src/main/java/renato/weather/carrot/com/carrot/LocationAdapter.java
@@ -1,11 +1,15 @@
 package renato.weather.carrot.com.carrot;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,27 +24,30 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
 {
 
 	private List<Location> locationList;
-
-	public LocationAdapter(List<Location> locationList)
+	private boolean isFavouriteList;
+	private Context context;
+	public LocationAdapter(List<Location> locationList, boolean isFavouriteList)
 	{
 		this.locationList = locationList;
-	}
+		this.isFavouriteList = isFavouriteList;
 
+	}
+	
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
 	{
-		Context context = parent.getContext();
+		context = parent.getContext();
 		View locationItem = LayoutInflater.from(context).inflate(R.layout.view_location_item, parent, false);
 		return new ViewHolder(locationItem);
 	}
-
+	
 	@Override
 	public void onBindViewHolder(ViewHolder holder, final int position)
 	{
-		Location searchLocation = locationList.get(position);
-		holder.locationCity.setText(searchLocation.getName());
+		final Location searchLocation = locationList.get(position);
 		
-		holder.itemView.setOnClickListener(new View.OnClickListener()
+		holder.locationCity.setText(searchLocation.getName());
+		holder.locationCity.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
@@ -48,26 +55,59 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
 				EventBus.getInstance().post(new LocationChangeEvent(locationList.get(position)));
 			}
 		});
+		
+		holder.addButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				if(isFavouriteList)
+				{
+					remove(position);
+				}
+				else
+				{
+					add(searchLocation);
+				}
+			}
+		});
+		
+		holder.setToggleIcon(isFavouriteList);
 	}
-
+	
 	@Override
 	public int getItemCount()
 	{
-		//TODO replace this value with something more relevant
 		return locationList.size();
 	}
-
+	
 	public static class ViewHolder extends RecyclerView.ViewHolder
 	{
 		private TextView locationCity;
-
+		private Button addButton;
 		public ViewHolder(View itemView)
 		{
 			super(itemView);
-
+			
 			locationCity = (TextView) itemView.findViewById(R.id.city_text);
-
+			addButton = (Button) itemView.findViewById(R.id.add_button);
+			
 		}
-
+		
+		public void setToggleIcon(boolean isFavouriteList)
+		{
+			
+			addButton.setBackgroundResource(isFavouriteList ? R.drawable.ic_trash: R.drawable.ic_add);
+		}
+	}
+	
+	private void add(Location location)
+	{
+		LocationManager.getInstance(context).addLocation(location);
+	}
+	
+	private void remove(int position)
+	{
+		LocationManager.getInstance(context).removeLocation(position);
 	}
 }
