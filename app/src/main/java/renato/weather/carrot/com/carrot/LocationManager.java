ef0537264c6f6fe2734964f24c3acb0d6dd624c8
Renato Mussa <rfmussa@gmail.com>
2015-01-15 14:42:09 +0100

    Small changes on the LocationManager class

--- app/src/main/java/renato/weather/carrot/com/carrot/LocationManager.java
+++ app/src/main/java/renato/weather/carrot/com/carrot/LocationManager.java
@@ -3,6 +3,7 @@ package renato.weather.carrot.com.carrot;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -12,10 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import renato.weather.carrot.com.carrot.events.EventBus;
-import renato.weather.carrot.com.carrot.events.LocationChangeEvent;
 import renato.weather.carrot.com.carrot.events.SavedLocationChange;
 import renato.weather.carrot.com.carrot.rest.model.Location;
-import renato.weather.carrot.com.carrot.rest.model.Locations;
 
 /**
  * Created by Renato on 13/01/15.
@@ -27,8 +26,8 @@ public class LocationManager
 	private SharedPreferences preferences;
 	private Gson gson;
 	private SharedPreferences.Editor editor;
-	private List<Location> savedLocations = new ArrayList<>();
-
+	private List<Location> savedLocations;
+	private Context context;
 
 	public static LocationManager getInstance(Context context)
 	{
@@ -41,6 +40,7 @@ public class LocationManager
 	
 	public LocationManager(Context context)
 	{
+		this.context = context;
 		gson = new Gson();
 		preferences = PreferenceManager.getDefaultSharedPreferences(context);
 		editor = preferences.edit();
@@ -52,7 +52,7 @@ public class LocationManager
 	{
 		String locationsString = gson.toJson(savedLocations);
 		editor.putString(LOCATIONS_ID, locationsString);
-		editor.commit();
+		editor.apply();
 		EventBus.getInstance().post(new SavedLocationChange());
 
 	}
@@ -64,15 +64,19 @@ public class LocationManager
 		}
 		savedLocations.add(location);
 		update();
-		
+		Log.d("Location Added", location.getName());
 	}
 	
 	public void removeLocation(int position)
 	{
-		savedLocations.remove(position);
-		update();
+		if(savedLocations != null)
+		{
+			String locationName = savedLocations.get(position).getName();
+			savedLocations.remove(position);
+			update();
+			Log.d("Location Removed", locationName);
+		}
 	}
-	
 	
 	// deserialize
 	public List<Location> getLocations()
@@ -81,7 +85,7 @@ public class LocationManager
 		{
 			return savedLocations;
 		}
-
+		
 		String locationsString = preferences.getString(LOCATIONS_ID, null);
 		Type type = new TypeToken< List < Location >>() {}.getType();
 		savedLocations = new Gson().fromJson(locationsString, type);
